768a3b4f7c550af6b74cdf070e7fc6beda980449
Hisako <52304998+ZannaSkull@users.noreply.github.com>
2023-05-30 01:21:13 +0200

    Update Valery.py
    Everything Revamp.

--- Valery.py
+++ Valery.py
@@ -1,36 +1,54 @@
-import random
-import asyncio
-import aiohttp
+from pypresence import Presence
 from plyer import notification
 import threading
+import aiohttp
+import asyncio
+import random
+import ctypes
+import os
 
 PINK = "\033[95m"
 RESET = "\033[0m"
+BOLD = "\033[1m"
+PURPLE = "\033[35m"
 
-ascii_text = r""" {pink}
+title = "Valery"
+title_bytes = title.encode('cp1252') 
+ctypes.windll.kernel32.SetConsoleTitleA(title_bytes)
 
-██╗   ██╗ █████╗ ██╗     ███████╗██████╗ ██╗   ██╗
-██║   ██║██╔══██╗██║     ██╔════╝██╔══██╗╚██╗ ██╔╝
-██║   ██║███████║██║     █████╗  ██████╔╝ ╚████╔╝ 
-╚██╗ ██╔╝██╔══██║██║     ██╔══╝  ██╔══██╗  ╚██╔╝  
- ╚████╔╝ ██║  ██║███████╗███████╗██║  ██║   ██║   
-  ╚═══╝  ╚═╝  ╚═╝╚══════╝╚══════╝╚═╝  ╚═╝   ╚═╝   
-        The Lead of WebHook Spammers
+RPC = Presence(client_id="1112872663384326154")
+RPC.connect() 
+RPC.update(
+    state="Valery | Spamming Webhooks",
+    details="The Best Free WebHook Spammer!",
+    large_image="valerylarge",
+    large_text="Using The Lead of WebHook Spammers ❤ "
+)
+
+ascii_text = r""" {bold}{pink}
+
+██╗   ██╗ █████╗ ██╗     ███████╗██████╗ ██╗   ██╗      ⠀⠀⠀⠀⠀⠀⠀⠀⣠⣶⣶⣶⣦⠀⠀
+██║   ██║██╔══██╗██║     ██╔════╝██╔══██╗╚██╗ ██╔╝⠀⠀     ⣠⣤⣤⣄⣀⣾⣿⠟⠛⠻⢿⣷⠀
+██║   ██║███████║██║     █████╗  ██████╔╝ ╚████╔╝     ⢰⣿⡿⠛⠙⠻⣿⣿⠁⠀⠀⠀⢸⣿⡇               
+╚██╗ ██╔╝██╔══██║██║     ██╔══╝  ██╔══██╗  ╚██╔╝      ⢿⣿⣇⠀⠀⠀⠈⠏⠀⠀⠀⠀⣼⣿⠀
+ ╚████╔╝ ██║  ██║███████╗███████╗██║  ██║   ██║   ⠀   ⠻⣿⣷⣮⣤⣀⠀⠀⠀⠀⣾⡿⠃⠀
+  ╚═══╝  ╚═╝  ╚═╝╚══════╝╚══════╝╚═╝  ╚═╝   ╚═╝   ⠀⠀⠀⠀   ⠉⠉⠻⣿⣄⣴⣿⠟⠀⠀
+        The Lead of WebHook Spammers⠀⠀⠀⠀⠀⠀⠀                 ⣿⡿⠟⠁⠀⠀
               Hisako Made This.                                  
+{reset} """.format(bold=BOLD, pink=PINK, reset=RESET)
 
-{reset} """.format(pink=PINK, reset=RESET)
-
+os.system("cls" if os.name == "nt" else "clear")
 print(ascii_text)
-message = input("Type message: ")
-webhook_url = input("Enter webhook: ")
-delay = int(input("Enter a delay: (0-5) "))
-threads_count = int(input("Enter the number of threads: "))
-messages_count = int(input("Enter the number of messages to spam: "))
-timeout = float(input("Enter the request timeout (in seconds): "))
+message = input(PURPLE + "Type message: " + RESET)
+webhook_url = input(PURPLE + "Enter webhook: " + RESET)
+delay = int(input(PURPLE + "Enter a delay: (0-5) " + RESET))
+threads_count = int(input(PURPLE + "Enter the number of threads: " + RESET))
+messages_count = int(input(PURPLE + "Enter the number of messages to spam: " + RESET))
+timeout = float(input(PURPLE + "Enter the request timeout (in seconds): " + RESET))
 
-use_proxies = input("Do you want to use proxies? (Y/N): ")
+use_proxies = input(PURPLE + "Do you want to use proxies? (Y/N): " + RESET)
 if use_proxies.lower() == "y":
-    proxy_file_path = input("Enter the path to the proxy file: ")
+    proxy_file_path = input(PURPLE + "Enter the path to the proxy file: " + RESET)
     with open(proxy_file_path, "r") as file:
         proxy_list = [proxy.strip() for proxy in file.readlines()]
 else:
@@ -52,8 +70,9 @@ async def send_webhook(webhook, msg, proxy):
                     print("\033[95mWebhook sent successfully.\033[0m")
                     return True
                 elif response.status == 429:
-                    print("\033[91mRate limit reached. Waiting for retry...\033[0m")
-                    await asyncio.sleep(float(response.headers.get("Retry-After", "5")))
+                    retry_after = float(response.headers.get("Retry-After", "5"))
+                    print("\033[91mRate limit reached. Waiting for retry... ({} seconds)\033[0m".format(retry_after))
+                    await asyncio.sleep(retry_after)
                     return False
                 else:
                     print("\033[91mFailed to send webhook. Status code:", response.status, "\033[0m")
@@ -92,6 +111,7 @@ async def spam_webhooks():
             message="The spamming process has finished. Sent: {} Webhooks".format(sent_count),
             app_icon=None
         )
+        print("\033[92mSuccessfully Valery Spammed\033[0m")
 
 def run_spamming():
     try:
@@ -101,6 +121,8 @@ def run_spamming():
 
     except Exception as e:
         print("An error occurred:", e)
+
+    RPC.close()
 
 threads = []
 for _ in range(threads_count):
